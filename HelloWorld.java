85ebb9a2ff307f479a373b0d043520fc24dde846
ucef81 <sa.youssef@gmail.com>
2022-02-01 16:29:52 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        try {
+       
         System.out.println("Hello World");
 
-        } catch(Exception e ){
-            System.out.println(e);
-        }
-            
+        
 }
