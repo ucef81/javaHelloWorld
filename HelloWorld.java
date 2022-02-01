b1afbb5aa6aa8a0930df86d51a1bb9ed506169be
ucef81 <sa.youssef@gmail.com>
2022-02-01 15:37:17 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+        try {
         System.out.println("Hello World");
-    }
+
+        } catch(Exception e ){
+            System.out.println("Erreur d'index");
+        }
+            
 }
